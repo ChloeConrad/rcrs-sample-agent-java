6dda319d6ded8da43520d14d4eb57191ae7af103
Chloé Conrad <chloeconrad@outlook.fr>
2022-04-03 19:35:42 +0200

    algo for path finding in AntSearch class

--- src/main/java/sampleagent/AntSearch.java
+++ src/main/java/sampleagent/AntSearch.java
@@ -1,76 +1,112 @@
 package sampleagent;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import rescuecore2.misc.collections.LazyMap;
+import rescuecore2.standard.entities.Area;
+import rescuecore2.standard.entities.Building;
 import rescuecore2.standard.entities.StandardWorldModel;
+import rescuecore2.worldmodel.Entity;
 import rescuecore2.worldmodel.EntityID;
 import sample.SampleSearch;
 
 
-public class AntSearch extends SampleSearch {
+public class AntSearch {
 	
-	 public AntSearch(StandardWorldModel world) {
-		super(world);
-	}
+	/**
+	 * Map associant à chaque entités un set d'entités voisins 
+	 */
+	protected Map<EntityID, Set<EntityID>> graph;
+	private Set<EntityID>                buildingSet;
 
-	public List<EntityID> breadthFirstSearchForBlokade( EntityID start,
-		      Collection<EntityID> goals ){
-		 List<EntityID> open = new LinkedList<EntityID>();
-		    Map<EntityID, EntityID> ancestors = new HashMap<EntityID, EntityID>();
-		    open.add( start );
-		    EntityID next = null;
-		    boolean found = false;
-		    ancestors.put( start, start );
-		    do {
-		      next = open.remove( 0 );
-		      if ( isGoal( next, goals ) ) {
-		        found = true;
-		        break;
+	
+	public AntSearch(StandardWorldModel world ) {
+		  Map<EntityID, Set<EntityID>> neighbours = new LazyMap<EntityID, Set<EntityID>>() {
+
+		      @Override
+		      public Set<EntityID> createValue() {
+		        return new HashSet<EntityID>();
 		      }
-		      Collection<EntityID> neighbours = get( next );
-		      if ( neighbours.isEmpty() ) {
-		        continue;
+		    };
+		    buildingSet = new HashSet<EntityID>();
+		    for ( Entity next : world ) {
+		      if ( next instanceof Area ) {
+		        Collection<EntityID> areaNeighbours = ( (Area) next ).getNeighbours();
+		        neighbours.get( next.getID() ).addAll( areaNeighbours );
+		        if ( next instanceof Building ) 
+		        	buildingSet.add( next.getID() );
 		      }
-		      for ( EntityID neighbour : neighbours ) {
-		        if ( isGoal( neighbour, goals ) ) {
-		          ancestors.put( neighbour, next );
-		          next = neighbour;
-		          found = true;
-		          break;
-		        } else {
-		          if ( !ancestors.containsKey( neighbour ) ) {
-		            open.add( neighbour );
-		            ancestors.put( neighbour, next );
-		          }
-		        }
-		      }
-		    } while ( !found && !open.isEmpty() );
-		    if ( !found ) {
-		      // No path
-		      return null;
 		    }
-		    // Walk back from goal to start
-		    EntityID current = next;
-		    List<EntityID> path = new LinkedList<EntityID>();
-		    do {
-		      path.add( 0, current );
-		      current = ancestors.get( current );
-		      if ( current == null ) {
-		        throw new RuntimeException(
-		            "Found a node with no ancestor! Something is broken." );
-		      }
-		    } while ( current != start );
-		    return path;
-		 
-	 }
-	 
-	 public List<EntityID> breadthFirstSearchForCivilian( EntityID start,
+		    
+		    this.graph = neighbours;
+	}
+	
+	public List<EntityID> seachBySwarm( EntityID start,
 		      Collection<EntityID> goals ){
-		 return null;
+		List<EntityID> path = new ArrayList<EntityID>();
+		EntityID currentStart = start;
+		boolean findGoals = false;
+		
+		//Test si je suis sur un de mes objectifs 
+		if(goals.contains(start)) {
+			path.add(start);
+			start.pheromoneUpdate();
+			return path;
+		}
+		
+		
+		do {
+			Set<EntityID> possibilities = graph.get(currentStart);
+			EntityID nextStart = currentStart;
+			float maxProba = Float.MIN_VALUE;
+			for(EntityID voisin : possibilities) {
+				if(goals.contains(voisin)) {
+					findGoals = true;
+					nextStart = voisin;
+					break;
+				}
+				float proba = voisin.getPheromone()/pheromoneSomme(possibilities);
+				if(proba>maxProba) {
+					maxProba = proba;
+					nextStart = voisin;
+				}
+			}
+			
+			//Si aucun voisin n'a de phéromone, choisi un chemin random
+			if(maxProba==0)
+				nextStart = randomWay(possibilities);
+			path.add(nextStart);
+			
+		}while(!findGoals);
+		
+		//mise à jour des phéromones 
+		for(EntityID step : path) 
+			step.pheromoneUpdate();
+		
+		return path;
 		 
 	 }
+	
+	private EntityID randomWay(Set<EntityID> possibilities) {
+		EntityID[] possibilitiesTAb = (EntityID[]) possibilities.toArray();
+		int random = (int)Math.random()*possibilitiesTAb.length;
+		return possibilitiesTAb[random];
+	}
+	 
+	private float pheromoneSomme(Set<EntityID> entities) {
+		float res = 0;
+		for(EntityID entity : entities) 
+			res += entity.getPheromone();
+		return res;
+			
+	}
+	
+	
 }
